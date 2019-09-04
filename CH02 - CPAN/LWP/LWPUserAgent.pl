17dca2ad73fe275732e4abafaca88d35a0795297
Paul McQuade <paulmcquad@gmail.com>
2019-09-04 18:30:28 +0100

    Using CPAN Modules
    LWPUserAgent is mimic a web browser
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

---
+++ CH02 - CPAN/LWP/LWPUserAgent.pl
@@ -0,0 +1,27 @@
+use strict;
+use warnings;
+ 
+use HTTP::CookieJar::LWP ();
+use LWP::UserAgent       ();
+ 
+my $jar = HTTP::CookieJar::LWP->new;
+my $ua  = LWP::UserAgent->new(
+    cookie_jar        => $jar,
+    protocols_allowed => ['http', 'https'],
+    timeout           => 10,
+);
+ 
+$ua->env_proxy;
+ 
+my $response = $ua->get('https://www.coder-forge.com/');
+ 
+if ($response->is_success) {
+    print $response->decoded_content;
+}
+else {
+    die $response->status_line;
+}
+
+open(FILE, ">LWPUserAgent.txt");
+print FILE $response->{_content};
+close(FILE);
