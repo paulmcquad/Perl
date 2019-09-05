f2709de4010b4891fa4e4fec5b495af8f6e4d554
Paul McQuade <paulmcquad@gmail.com>
2019-09-05 18:50:38 +0100

    Added IOSocket script
    IO::Socket - Connect to a web server
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

---
+++ CH02 - CPAN/IO/IOSocket.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+# Connect to a Web Server
+
+print "Content-type: text/html\n\n";
+
+use IO::Socket;
+ 
+my $sock = IO::Socket::INET->new
+(
+ Proto      => "tcp",
+ PeerAddr   => "coder-forge.com",
+ PeerPort   =>  80
+);
+
+$sock->autoflush(1);
+
+print $sock "GET /resources.html HTTP/1.0\015\012\015\012";
+
+open(FILE, ">IOSocket.txt");
+while (<$sock>) {
+    print FILE;
+    }
+    close(FILE);
+
